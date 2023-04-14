4a0be39745a92a5965848952a7ea6e20997b32f9
muzahid-nayim <muzahidulislamnayim@gmail.com>
2023-04-14 09:56:04 +0600

    part 6

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 
 from flaskblog import app
+from flaskblog import db
+# from flaskblog.models import User, Post
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True,host="0.0.0.0",port=2000)
 
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
